6f683c03fc90d2bee2bdff4efb1022c2ed96190f
Atul-Bonde <atul.bonde@gmail.com>
2023-08-08 14:20:08 +0530

    Crud Operation Completed

--- src/main/java/com/RestAPI/query/Query.java
+++ src/main/java/com/RestAPI/query/Query.java
@@ -8,4 +8,13 @@ public class Query {
 	public static String  save="insert into product values(?,?,?,?)";
 
 	public static String byid="select * from product where id=?";
+	
+	public static String byname="select * from product where name=?";
+	
+	public static String updateprice ="update product set price=?  where name=? ";
+	
+	public static String deletebyname="delete from product where name=?";
+	
+	public static String getall="select * from product";
+	
 }
--- src/main/java/com/RestAPI/repository/ProductRepository.java
+++ src/main/java/com/RestAPI/repository/ProductRepository.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -14,56 +16,142 @@ import com.RestAPI.query.Query;
 
 @Repository
 public class ProductRepository {
-	
+
 	@Autowired
 	private Query query;
-	
+
 	@Autowired
 	private Configuration1 configuration1;
-	
+
 	public boolean save(Product p) throws ClassNotFoundException, SQLException {
-		
+
 		Connection con = configuration1.getconfi();
-		
+
 		String query = Query.save;
-		
+
 		PreparedStatement ps = con.prepareStatement(query);
-		
+
 		ps.setInt(1, p.getId());
 		ps.setString(2, p.getName());
 		ps.setInt(3, p.getQuantity());
 		ps.setDouble(4, p.getPrice());
-		
+
 		boolean execute = ps.execute();
-		
+
 		con.close();
-		
+
 		return execute;
-		
-		
+
 	}
-	
+
 	public Product findbyid(int id) throws ClassNotFoundException, SQLException {
-		
+
 		Connection con = configuration1.getconfi();
-		
-		String query=Query.byid;
-		
+
+		String query = Query.byid;
+
 		PreparedStatement ps = con.prepareStatement(query);
-		
+
 		ps.setInt(1, id);
-		
+
 		ResultSet rs = ps.executeQuery();
-		
-		Product p=new Product();
-		
-		while(rs.next()) {
-			
-			p=new Product(rs.getInt(1),rs.getString(2),rs.getInt(3),rs.getDouble(4));
-			
+
+		Product p = new Product();
+
+		while (rs.next()) {
+
+			p = new Product(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getDouble(4));
+
 		}
-		
+
 		return p;
+	}
+
+	public Product findbyname(String name) throws ClassNotFoundException, SQLException {
+
+		Connection con = configuration1.getconfi();
+
+		String query = Query.byname;
+
+		PreparedStatement ps = con.prepareStatement(query);
+
+		ps.setString(1, name);
+
+		ResultSet rs = ps.executeQuery();
+
+		Product p = new Product();
+
+		while (rs.next()) {
+
+			p = new Product(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getDouble(4));
+
+		}
+
+		con.close();
+
+		return p;
+
+	}
+
+	public Integer updateprice(String name, double price) throws ClassNotFoundException, SQLException {
+
+		Connection con = configuration1.getconfi();
+
+		String query = Query.updateprice;
+
+		PreparedStatement ps = con.prepareStatement(query);
+
+		ps.setDouble(1, price);
+		ps.setString(2, name);
+
+		int executeUpdate = ps.executeUpdate();
+
+		con.close();
+
+		return executeUpdate;
+
+	}
+
+	public Integer delete(String name) throws ClassNotFoundException, SQLException {
+
+		Connection con = configuration1.getconfi();
+
+		String query = Query.deletebyname;
+
+		PreparedStatement ps = con.prepareStatement(query);
+
+		ps.setString(1, name);
+
+		int delete = ps.executeUpdate();
+
+		return delete;
+
+	}
+
+	public ArrayList<Product> getallproduct() throws ClassNotFoundException, SQLException {
+
+		Connection con = configuration1.getconfi();
+
+		String query = Query.getall;
+
+		Statement st = con.createStatement();
+
+		ResultSet rs = st.executeQuery(query);
+
+		ArrayList<Product> al = new ArrayList<>();
+
+		Product p = new Product();
+
+		while (rs.next()) {
+
+			p = new Product(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getDouble(4));
+
+			al.add(p);
+
+		}
+
+		return al;
+
 	}
 
 }
